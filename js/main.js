a98a687f0e0cff0b25956533ac403910e10e496f
dvorit-ai <dkhodanovich@gmail.com>
2020-05-18 14:16:34 -0400

    work work work
    index.html done (??) for now
    cleaned up code & added comments

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
- 		var draw = SVG().addTo('#background').size($(window).width(), $(window).height())
+ 		var draw = SVG().addTo('#backgroundX').size($(window).width(), $(window).height())
 
- 		var line1 = draw.line($(window).width(), 0, 0, $(window).height()).stroke({ width: 2, color:"#0F0" })
+ 		var line1 = draw.line($(window).width(), 0, 0, $(window).height()).stroke({ width: 1, color:"#0F0" })
 
-		var line2 = draw.line(0, 0, $(window).width(), $(window).height()).stroke({ width: 2, color:"#0F0" })
+		var line2 = draw.line(0, 0, $(window).width(), $(window).height()).stroke({ width: 1, color:"#0F0" })
