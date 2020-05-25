b50af010b5d5fe588367e41b12a3ca04a8e99b73
alisq <ali.shamas.qadeer@gmail.com>
2020-05-25 17:22:12 -0400

    filters working

--- public/js/main.js
+++ public/js/main.js
@@ -4,12 +4,15 @@
 
 		// var line2 = draw.line(0, 0, $(window).width(), $(window).height()).stroke({ width: 1, color:"#0F0" })
 
-		var projectTeasers = [];
-		var projects = [];
-		var studentList = [];
+		var projectTeasers = []; //this array will hold all the HTML for the Thubnails on the front page
+		var projects = [];			//this array will hold all the HTML for the full projects.
+		var studentList = [];		//this array will list all the student names and projects
+		var allTags = [];			//this array will hold all the unique tags
 
  
 
+
+//this is function to randomize any array
 function shuffle(array) {
   for (let i = array.length - 1; i > 0; i--) {
     let j = Math.floor(Math.random() * (i + 1));
@@ -18,6 +21,7 @@ function shuffle(array) {
 }
 
 
+	//this reaches out the the JSON file and pulls in all the data
 	fetch('data/responses.json')
 	  .then(response => response.json())
 	  .then(data => {
@@ -26,11 +30,20 @@ function shuffle(array) {
 
 
 
-
+	  	//here we will loop through ALLLLL the data.
 	  	for (i=0; i<data.length;i++) {
 	  	//console.log(data[i]["Thumbnail Image"])	
 
-	  	var tags="<h6 class='tag'>"+data[i]["Project Tags"].replace(/;/g,"</h6><br /><h6 class='tag'>")+"</h6>";
+	  	var tags=data[i]["Project Tags"].split(";");
+	  	var tagButtons = "";
+	  	var tagClasses= ""
+	  	
+	  			for (t=0;t<tags.length;t++) {
+	  				tagClasses += "t_"+tags[t].toLowerCase().replace(" ","")+" ";
+	  				tagButtons += "<h6 class='tag'>"+tags[t]+"</h6>";
+	  				allTags.push(tags[t]);
+	  				
+	  			}
 
 
 	  	var imagenames = data[i]["First & Last Name"].toLowerCase().replace(" ","_")
@@ -45,13 +58,13 @@ function shuffle(array) {
 
 
 
-		var projTeaser = 	"<div id='project-"+i+"' class=\"thumbnail\">"+
+		var projTeaser = 	"<div id='project-"+i+"' class=\"thumbnail "+tagClasses+"\"+>"+
 					"<a data-project-id='"+i+"' id='project-link-"+i+"' class=\"project-link\" data-url="+data[i]["First & Last Name"].toLowerCase().replace(" ","_")+">"+
 					"<img src=\"images/"+thumb+"\" alt=\"\"/>"+
 					"<div class=\"img-tags\">"+
 					"<p class='uppercase'>"+data[i]["First & Last Name"]+"</p>"+
 					"<h3>"+data[i]["Project Title"]+"</h3>"+
-					tags+
+					tagButtons+
 					"</div>"+
 					"</a>"+
 					"</div>";
@@ -65,7 +78,7 @@ function shuffle(array) {
 "<p class='uppercase'>"+data[i]["First & Last Name"]+"</p>"+
 "<h3>"+data[i]["Project Title"]+"</h3>"+
 "<p class=\"description\">"+data[i]["Project Description"]+"</p>"+
-"<p class=\"uppercase description\">"+tags+"</p>"+
+"<p class=\"uppercase description\">"+tagButtons+"</p>"+
 "<img src=\"images/"+portrait+"\">"+
 
 "<p class=\"uppercase\">"+"About the designer"+"</p>"+
@@ -105,6 +118,21 @@ function shuffle(array) {
 		"<h6 class=\"caption\">"+data[i]["Caption #5: OPTIONAL"]+"</h6>"
 	}
 
+
+
+
+	if (data[i]["Video Link: OPTIONAL"] != undefined) {
+		if (data[i]["Video Link: OPTIONAL"].includes("vimeo")) {
+			var l = data[i]["Video Link: OPTIONAL"].split(".com/")[1];		
+			projFull += "<style>.embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style><div class='embed-container'><iframe src='https://player.vimeo.com/video/"+l+"' frameborder='0' webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe></div>"
+		} else {
+			var l = data[i]["Video Link: OPTIONAL"].split(".com/")[1];		
+			projFull += "<iframe title='vimeo-player' src='https://player.vimeo.com/video/"+l+"' width='640' height='360' frameborder='0' allowfullscreen></iframe>"
+		}
+		projFull+="<h6 class=\"caption\">"+data[i]["Video Caption: OPTIONAL"]+"</h6>"
+		
+	}
+
 	projFull += "</div></div>"
 
 
@@ -137,13 +165,40 @@ function shuffle(array) {
 		}
 
 
+		var uniqueTags = [];
+		$.each(allTags, function(i, el){
+		    if($.inArray(el, uniqueTags) === -1) uniqueTags.push(el);
+		});
+		uniqueTags.sort();
+		for (t=0;t<uniqueTags.length;t++) {
+			$(".filter-by-tag").append("<h6 class='tag t_"+uniqueTags[t].toLowerCase().replace(" ","")+"'>"+uniqueTags[t]+"</h6>")
+		}
+
 		setTimeout(function(){
 	  		$(".projects-container").fadeIn(200);
 	  	},500)
 	  });
 
 
+ 	$(document).on("click",".filter-by-tag .tag",function(){
+ 		$(".tag").removeClass("active");
+ 		$(this).addClass("active");
+ 		cT = "t_"+$(this).text().toLowerCase().replace(" ",""); 
+		$("."+cT).fadeIn(200)
+		$(".thumbnail:not(."+cT+")").fadeOut(200)
+		history.pushState({
+		    id: $(this).data("url")
+		}, $(this).data("url"), "/?tag="+cT);
+ 	})
 
+
+ 	$(document).on("click",".filter",function(){
+ 		$(".tag").removeClass("active")
+		$(".thumbnail").fadeIn(200)
+		history.pushState({
+		    id: $(this).data("url")
+		}, $(this).data("url"), "/");
+ 	})
 
 
 
@@ -180,12 +235,16 @@ function shuffle(array) {
 	  				var getz = u.split("?")[1].split("=");
 	  				if (getz[0] == "id") {		
 	  					$("#project-link-"+getz[1]).click();
+	  				} else if (getz[0] == "tag") {		
+	  					rrr = ".filter-by-tag ."+getz[1].toLowerCase().replace(" ","");
+	  					console.log(rrr)
+	  					$(rrr).click();
 	  				}
 	  			}
 	  			
 	  		
 
-	  	},1000)
+	  	},500)
 
 	  	$(".projects-container").fadeOut(200);
 
