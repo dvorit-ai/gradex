044700546a2d28eb6d7c07ce6095c009d42893d5
alisq <ali.shamas.qadeer@gmail.com>
2020-06-04 17:03:32 -0400

    alert!

--- public/js/main.js
+++ public/js/main.js
@@ -470,7 +470,7 @@ $(document).on("click",".filter",function(e){
 	$(".filter-by-tag").toggleClass("active")
 	$(document).scrollTop(0)
 
-	//if (window.location.href)
+	alert(window.location.href)
 })
 
  	$(document).on("click",".filter-by-tag .tag",function(){
